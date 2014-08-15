9cb4780516b0cfd993df21f17be37a4bd7007160
John Swoboda <swoboj@bu.edu>
2014-08-15 10:32:36 -0400

    updata radarsystools to work with command line arguments.

--- radarsystools/radarsystools.py
+++ radarsystools/radarsystools.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 """
 radarsystools
-This set of functions can be used to calculate expect SNR and variance of ISR 
+This set of functions can be used to calculate expected SNR and RMS error of ISR 
 data.
 
 @author: John Swoboda
 """
-
-
+import sys, getopt
 import numpy as np
 
 from RadarDataSim.const.physConstants import v_C_0, v_Boltz, v_electron_rcs, v_epsilon0,v_elemcharge
@@ -64,14 +63,11 @@ class RadarSys(object):
             snrliststr = ['{:.2f}dB '.format(i) for i in SNRdb[irng]]
             snrliststr.insert(0,rngstr)
             lstoflst.append(snrliststr)
-        # Determine    
-        cols = zip(*lstoflst)
-        col_widths = [ max(len(value) for value in col) for col in cols ]
-        format = ' '.join(['%%-%ds' % width for width in col_widths ])
-        for row in lstoflst:
-            print format % tuple(row)
+        
+        printtable(lstoflst)
 
-    def printvar(self,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
+
+    def printrms(self,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
         '''This function will print out the variances esimtates of ISR data 
         with the input plasma parameters and system prameters. The inputs are
         the basic plasma parameters the impact the variance, the electron density and
@@ -90,9 +86,9 @@ class RadarSys(object):
         #%% Do the SNR calculation
         powdata = self.powcalc(ne,te,ti)
         vardata = (powdata+self.noisepow)**2/self.Kpulse
-        
+        rmsdata = np.sqrt(vardata)
         #%% Set up and Make print out
-        print "Variances for the follwing Ne with Number of pulses = {0:d}".format(Kpulse)
+        print "RMS for the follwing Ne with Number of pulses = {0:d}".format(Kpulse)
         # make strings for each Ne
         nestrs = ['{:.2g}m^-3 '.format(i) for i in ne]
         nestrs.insert(0,'Range ')
@@ -100,15 +96,11 @@ class RadarSys(object):
         # make a list of lists for each SNR
         for irng,vrng in enumerate(rng):
             rngstr = "{0:.2e}km ".format(vrng)
-            varliststr = ['{:.2e}W^2 '.format(i) for i in vardata[irng]]
-            varliststr.insert(0,rngstr)
-            lstoflst.append(varliststr)
-        # Determine    
-        cols = zip(*lstoflst)
-        col_widths = [ max(len(value) for value in col) for col in cols ]
-        format = ' '.join(['%%-%ds' % width for width in col_widths ])
-        for row in lstoflst:
-            print format % tuple(row)
+            rmsliststr = ['{:.2e}W '.format(i) for i in rmsdata[irng]]
+            rmsliststr.insert(0,rngstr)
+            lstoflst.append(rmsliststr)
+        printtable(lstoflst)
+
             
     def printfracrms(self,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
         '''This function will print out the Fractional RMS esimtates of ISR data 
@@ -142,12 +134,8 @@ class RadarSys(object):
             rmsliststr = ['{:.2f} '.format(i) for i in rmsdata[irng]]
             rmsliststr.insert(0,rngstr)
             lstoflst.append(rmsliststr)
-        # Determine    
-        cols = zip(*lstoflst)
-        col_widths = [ max(len(value) for value in col) for col in cols ]
-        format = ' '.join(['%%-%ds' % width for width in col_widths ])
-        for row in lstoflst:
-            print format % tuple(row)
+
+        printtable(lstoflst)
             
     def powcalc(self,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
         '''This function will print out the returned power esimtates of ISR data 
@@ -190,52 +178,63 @@ class RadarSys(object):
             rcs =NE/((1.0+k**2*debyel**2)*(1.0+k**2*debyel**2+Tr))
         Pr = pt1*pt2*rcs
         return Pr
-#def snrcalc(sysdict,rng = np.arange(100.0,600.0,100.0),Kpulse = 1,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
-#    
-#    assert type(te) is np.ndarray, "te is not an numpy array."
-#    assert type(ne) is np.ndarray, "ne is not an numpy array."
-#    assert type(ti) is np.ndarray,  "ti is not an numpy array."
-#    powdata = powcalc(sysdict,rng,ne,te,ti)
-#    noisedata = noisepow(sysdict['Tsys'],sysdict['BandWidth'])
-#    
-#    return np.sqrt(Kpulse)*powdata/noisedata
-#    
-#    
-#def varcalc(sysdict,rng=np.arange(100.0,600.0,100.0),Kpulse = 1,ne = np.array([1e11]),te=np.array([1e3]),ti=np.array([1e3])):
-#    assert type(te) is np.ndarray, "te is not an numpy array."
-#    assert type(ne) is np.ndarray, "ne is not an numpy array."
-#    assert type(ti) is np.ndarray,  "ti is not an numpy array."
-#    powdata = powcalc(sysdict,rng,ne,te,ti)
-#    noisedata = noisepow(sysdict['Tsys'],sysdict['BandWidth'])
-#    return (powdata+noisedata)**2/Kpulse
-    
 
 #%% Utility functions   
 def noisepow(Tsys,BW):
     return Tsys*BW*v_Boltz
 def pow2db(x):
+    """Returns in dB x."""
     return 10.0*np.log10(x)
 def mag2db(x):
-    return 20.0*np.log10(x)
-#%% Test function
-if __name__== '__main__':
+    """Returns in dB the magnitude in x."""
+    return 20.0*np.log10(np.abs(x))
+def printtable(rows):
+    cols = zip(*rows)
+    col_widths = [ max(len(value) for value in col) for col in cols ]
+    format = ' '.join(['%%-%ds' % width for width in col_widths ])
+    for row in rows:
+        print format % tuple(row)
+        
+def main(argv):
     
+    kinput=1
+    try:
+      opts, args = getopt.getopt(argv,"hp:o:",["pulses=","ofile="])
+    except getopt.GetoptError:
+      print 'radarsystools.py -p <number of pulses> -o <outputfile>'
+      sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+         print 'radarsystools.py -p <number of pulses> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-p", "--pulses"):
+         kinput = int(arg)
+      elif opt in ("-o", "--ofile"):
+          print "Still have to add output file feature. I'm working on it... may be..."
+         
     print "Without angle"
     sensdict = sensconst.getConst('risr')
     sensdict['tau'] = 320e-6
-    r_sys = RadarSys(sensdict,Kpulse = 100)
+    r_sys = RadarSys(sensdict,Kpulse = kinput)
     r_sys.printsnr(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
-    r_sys.printvar(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
+    r_sys.printrms(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
     r_sys.printfracrms(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
     print "\n\n"
+    
+    
     angles = [(90,85)]
     ang_data = np.array([[iout[0],iout[1]] for iout in angles])
     sensdict = sensconst.getConst('risr',ang_data)
     sensdict['tau'] = 320e-6
 
     print "With Angle"
-    r_sys = RadarSys(sensdict,Kpulse = 100)
+    r_sys = RadarSys(sensdict,Kpulse = kinput)
     r_sys.printsnr(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
-    r_sys.printvar(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
+    r_sys.printrms(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
     r_sys.printfracrms(ne = np.array([1e11,2e11]),te=np.array([1e3,2e11]),ti=np.array([1e3,2e11]))
+    
+#%% Test function
+if __name__== '__main__':
+    main(sys.argv[1:])
+    
     
