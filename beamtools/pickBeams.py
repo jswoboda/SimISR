6b131143b3d2ddd3193136d668926f18fa976ff1
John Swoboda <swoboj@bu.edu>
2015-09-08 12:56:49 -0400

    Added changes from Greg

--- beamtools/pickBeams.py
+++ beamtools/pickBeams.py
@@ -6,13 +6,13 @@ the ability to switch between PFISR and RISR-N has been added along with a finis
 The code also outputs a picture of the selected beam pattern.
 
 @author: John Swoboda
+Updated by Greg Starr so it can be used as part of a larger GUI
 """
 
 from Tkinter import *
 import os, inspect
 import numpy as np
 import matplotlib.pyplot as plt
-import pdb
 from beamfuncs import BeamSelector
 
 def rect(r, w, deg=1):
@@ -32,38 +32,47 @@ def polar(x, y, deg=1):
 
 
 class Gui():
-    def __init__(self,root):
+    def __init__(self,parent,subgui=True):
+         # get the current path
         curpath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        self.root = root
-        self.root.title("Beam Selector")
+        # set the root
+        self.parent = parent
+        # set up frames for list
+        self.frame1 = Frame(self.parent)
+        self.frame1.grid(row=0,column=0)
+        self.frame2 = Frame(self.parent)
+        self.frame2.grid(row=0,column=1)
 
         self.output = []
         self.beamhandle = None
-        self.sizecanv = [1000,1000]
+        if subgui:
+            self.sizecanv = [500,500]
+        else:
+            self.sizecanv = [1000,1000]
+            self.butt = Button(self.frame1, text="Finished", command=self.buttonClick)
+            self.butt.grid(row=1,column=1,sticky='w')
+            self.leb = Label(self.frame1, text="Beam Selector",font=("Helvetica", 16))
+            self.leb.grid(row=0, sticky=W+E+N+S,columnspan=2)
+
+
         self.off_x = self.sizecanv[0]/2
         self.off_y = self.sizecanv[1]/2
         self.div = 75.0*self.sizecanv[0]/1000.0
         self.lat = [80,70,60,50,40,30]
         self.angles = np.arange(0,180,30)
-        self.frame = Frame(self.root,background="white")
-        self.frame.grid(row=0,column=0, sticky="n")
 
-        self.leb = Label(self.frame, text="Beam Selector",font=("Helvetica", 16))
-        self.leb.grid(row=0, sticky=W+E+N+S,columnspan=2)
         self.var = StringVar()
         self.var.set("PFISR")
         self.choices = {"PFISR":os.path.join(curpath,'PFISRbeammap.txt'), "RISR-N":os.path.join(curpath,'RISRNbeammap.txt')}#, "RISR-S":'file3'}
-        self.option = OptionMenu(self.frame, self.var, *self.choices)
+        self.option = OptionMenu(self.frame1, self.var, *self.choices)
         self.option.grid(row=1,column=0,sticky='w')
         self.lines = np.loadtxt(self.choices[self.var.get()])
 
 
-        self.butt = Button(self.frame, text="Finished", command=self.buttonClick)
-        self.butt.grid(row=1,column=1,sticky='w')
         self.readfile = StringVar()
 
         # set up the canvas
-        self.canv = Canvas(self.frame , width=self.sizecanv[0], height=self.sizecanv[1],background='white')
+        self.canv = Canvas(self.frame1 , width=self.sizecanv[0], height=self.sizecanv[1],background='white')
         self.canv.grid(row=2,column=0,columnspan=2)
 
         self.Drawlines()
@@ -74,9 +83,27 @@ class Gui():
         self.var.trace('w', self.Changefile)
         self.canv.update()
 
+        # beam list
+        self.bidlabel = Label(self.frame2,text="Beam ID")
+        self.bidlabel.grid(row=0,column=0)
+        self.azlabel = Label(self.frame2,text="Azimuth")
+        self.azlabel.grid(row=0,column=1)
+        self.ellabel = Label(self.frame2,text="Elevation")
+        self.ellabel.grid(row=0,column=2)
+
+        self.scroll = Scrollbar(self.frame2)
+        self.scroll.grid(row=1,column=3)
+
+        self.beamtext = Text(self.frame2,yscrollcommand=self.scroll.set)
+        self.beamtext.config(width=30,state=DISABLED)
+        self.beamtext.grid(row = 1,column = 0,columnspan=3)
+
+        self.scroll.config(command=self.beamtext.yview)
+
     def Changefile(self,*args):
         """ This function will change the files to a different radar system."""
         filename= self.choices[self.var.get()]
+        self.beamtext.delete(1.0,END)
         self.readfile.set(filename)
         self.lines = np.loadtxt(filename)
         self.output=[]
@@ -85,7 +112,7 @@ class Gui():
         self.Drawbeams()
 
     def Drawbeams(self):
-        "This function will "
+        "This function will draw all of the beams on the canvas."
         div =self.div
         #if self.beamhandle is not None:
 
@@ -106,7 +133,7 @@ class Gui():
             self.beamhandles.append(self.canv.create_oval(points, fill='blue',tags='beams'))
 
     def Drawlines(self):
-
+        """This function will draw all of the lines on the canvas for the """
         off_x = self.off_x
         off_y = self.off_y
         div = self.div
@@ -132,10 +159,9 @@ class Gui():
             self.canv.create_text(points[2],points[3], text=textangles[iang][1])
 
     def onCanvasClick(self,event):
-
+        """This function will find the nearest beam """
 
         p_coords = polar(event.x-self.off_x ,event.y-self.off_y)
-        print  'Clicked Az: ', p_coords[1],'El: ', np.abs(90 - p_coords[0]/6)
         x = self.canv.canvasx(event.x)
         y = self.canv.canvasy(event.y)
         dist = (self.ovalx-x)**2+(self.ovaly-y)**2
@@ -146,25 +172,27 @@ class Gui():
             self.canv.itemconfig(self.canv.find_withtag('header'),text=textheader)
             self.canv.itemconfig(self.beamhandles[linesit], fill='orange')
 
-            print '%s) Closest beam is # %s, Az: %s, El: %s' %(len(self.output)+1,int(closest[0]),closest[1],closest[2])
             self.output.append(closest[0])
             self.canv.update()
+
+            self.beamtext.config(state=NORMAL)
+            self.beamtext.insert(INSERT,"{:>9} {:>9} {:>9}\n".format(closest[0],closest[1],closest[2]))
+            self.beamtext.config(state=DISABLED)
         else:
-            print "Repeated beam"
+            print("Repeated beam")
+
     def buttonClick(self):
-        print 'Finished'
-        print self.output
+        """This will output the beam list, create an image of the beams and close the program. """
         allbeam = BeamSelector(self.lines)
         fig = allbeam.plotbeams(self.output,True,'beampic.png',"Chosenbeams")
         plt.close(fig)
         f = open('SelectedBeamCodes.txt', 'w')
         for beam in self.output:
             f.write("%s\n" % (int(beam)))
-        self.root.quit()
-        self.root.destroy()
-        #sys.exit()
+        sys.exit()
 
     def onCanvasRightClick(self,event):
+        """This will undo the choice of the nearest highlighted beam."""
         x = self.canv.canvasx(event.x)
         y = self.canv.canvasy(event.y)
         dist = (self.ovalx-x)**2+(self.ovaly-y)**2
@@ -174,13 +202,14 @@ class Gui():
             self.canv.itemconfig(self.beamhandles[linesit], fill='blue')
             self.output.remove(closest[0])
             self.canv.update()
-def run_beam_gui():
-    root = Tk()
-    gui = Gui(root)
-    root.mainloop()
 
+def run_beam_gui():
+    """Used to run the GUI as a function"""
+    root = Tk()
+    gui = Gui(root,False)
+    root.mainloop()
 if __name__ == "__main__":
 
     root = Tk()
-    gui = Gui(root)
+    gui = Gui(root,False)
     root.mainloop()
