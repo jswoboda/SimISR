Fixed method for error calculation, now uses full cove matrix

RadarDataSim/fitterMethodGen.py
@@ -46,8 +46,11 @@ class Fitterionoconainer(object):
         Ne = sp.absolute(self.Iono.Param_List[:,:,0]*(1.0+Tratio))
         if self.sig is None:
             Nesig = None
-        else:                    
-            Nesig = sp.absolute(self.sig.Param_List[:,:,0]*(1.0+Tratio))
+        else:
+            if self.sig.Param_List.ndim==4:
+                Nesig = sp.absolute(self.sig.Param_List[:,:,0,0]*(1.0+Tratio)**2)
+            elif self.sig.Param_List.ndim==3:
+                Nesig = sp.absolute(self.sig.Param_List[:,:,0]*(1.0+Tratio)**2)
         return (Ne,Nesig)
     def fitdata(self,fitfunc,startinputs,fittimes=None):
         """This funcition is used to fit data given in terms of lags """
@@ -77,15 +80,13 @@ class Fitterionoconainer(object):
         first_lag = True
         x_0all = startvalfunc(Ne_start,self.Iono.Cart_Coords,self.Iono.Time_Vector[:,0],startinputs)
         nparams=x_0all.shape[-1]
-        nx=nparams-1
         x_0_red = sp.zeros(4)
         specs = self.simparams['species']
         nspecs = len(specs)
         ni = nspecs-1
-
-        firstparam=True
+        sigexist = not self.sig is None
         L = self.sensdict['taurg']*2
-        dof = L-4
+        dof = 2*L-4
         if dof<=0:
             dof=1
         for itn,itime in enumerate(fittimes):
@@ -105,47 +106,54 @@ class Fitterionoconainer(object):
                     fittederror = sp.zeros((Nloc,Nt,nparams+1))
                     funcevals = sp.zeros((Nloc,Nt))
                 # get uncertianties
-                if not self.sig is None:
-                    if self.simparams['FitType'].lower()=='acf':
-                        # this is calculated from a formula
-                        d_func = d_func+(self.sig.Param_List[iloc,itime],)
-                    elif self.simparams['FitType'].lower()=='spectrum':
+                if sigexist:
+                    cursig = self.sig.Param_List[iloc,itime]
+                    if cursig.ndim<2:
+                        sigscov=sp.diag(cursig**2)
+                    else:
+                        sigscov=cursig
+                    if self.simparams['FitType'].lower()=='spectrum':
                         # these uncertianties are derived from the acf variances.
-                        acfvar = self.sig.Param_List[iloc,itime]**2
                         Nspec = self.simparams['numpoints']
                         #XXX when creating these variences I'm assuming the lags are independent
                         # this isn't true and I should use the ambiguity function to fix this.
-                        acfvarmat = sp.diag(acfvar)
+                        acfvarmat = sp.diag(sigscov)
                         # calculate uncertianties by applying the FFT to the columns and the
                         # ifft to the rows. Then multiply by the constant to deal with the different size ffts
-                        specmat = sp.ifft(sp.fft(acfvarmat,n=int(Nspec),axis=0),n=int(Nspec),axis=1)*Nspec**2/Nlags
-                        specsig = sp.sqrt(sp.diag(specmat.real))
-                        d_func = d_func+(specsig,)
+                        sigscov = sp.ifft(sp.fft(acfvarmat,n=int(Nspec),axis=0),n=int(Nspec),axis=1)*Nspec**2/Nlags
 
                 # Only fit Ti, Te, Ne and Vi
                 x_0_red[0]=Ti
                 x_0_red[1:] = x_0[2*ni:]
                 # Perform the fitting
-                (x,cov_x,infodict,mesg,ier) = scipy.optimize.leastsq(func=fitfunc,
-                    x0=x_0_red,args=d_func,full_output=True)
-                
-                funcevals[iloc,itn] = infodict['nfev']
+                optresults = scipy.optimize.least_squares(fun=ISRSfitfunction,x0=x_0_red,method='lm',verbose=0,args=d_func)
+                x=optresults.x
                 # Derive data for the ions using output from the fitter and ion species ratios which are assumed to be given.
                 ionstuff = sp.zeros(ni*2-1)
                 ionstuff[:2*ni:2]=x[1]*Niratio
                 ionstuff[1:2*ni-1:2] = x[0]
                 fittedarray[iloc,itn] = sp.append(ionstuff,sp.append(x,Ne_start[iloc,itime]))
-                # Check to make sure you get a covariance matrix
-                if cov_x is None:
-                    vars_vec = sp.ones(nparams)*float('nan')
-                else:
-                     # Derive covariances for the ions using output from the fitter and ion species ratios which are assumed to be given.
-                    vars_vec = sp.diag(cov_x)*(infodict['fvec']**2).sum()/dof
+#                fittedarray[iloc,itime] = sp.append(optresults.x,Ne_start[iloc,itime])
+                resid = optresults.cost
+                jac=optresults.jac
+                jacc = jac[0::2]+jac[1::2]
+                try:
+                    # Derive covariances for the ions using output from the fitter and ion species ratios which are assumed to be given.
+                    #covf = sp.linalg.inv(sp.dot(jac.transpose(),jac))*resid/dof
+
+                    if sigexist:
+                        covf = sp.linalg.inv(sp.dot(sp.dot(jacc.transpose(),sp.linalg.inv(sigscov)),jacc))
+                    else:
+                        covf = sp.linalg.inv(sp.dot(jac.transpose(),jac))*resid/dof
+                    vars_vec = sp.diag(covf).real
                     ionstuff = sp.zeros(ni*2-1)
                     ionstuff[:2*ni:2]=vars_vec[1]*Niratio
                     ionstuff[1:2*ni-1:2] = vars_vec[0]
                     vars_vec = sp.append(ionstuff,vars_vec)
+                except:
+                    vars_vec = sp.ones(nparams)*float('nan')
 
+#
                 if len(vars_vec)<fittederror.shape[-1]-1:
                     pdb.set_trace()
                 fittederror[iloc,itn,:-1]=vars_vec 

RadarDataSim/radarData.py
@@ -466,47 +466,59 @@ def lagdict2ionocont(DataLags,NoiseLags,sensdict,simparams,time_vec):
     pulsesnoise = sp.tile(NoiseLags['Pulses'][:,:,sp.newaxis],(1,1,Nlags))
     lagsNoise = lagsNoise/pulsesnoise
     lagsNoise = sp.tile(lagsNoise[:,:,sp.newaxis,:],(1,1,Nrng,1))
-
+    
 
 
     # subtract out noise lags
     lagsData = lagsData-lagsNoise
     # Calculate a variance using equation 2 from Hysell's 2008 paper. Done use full covariance matrix because assuming nearly diagonal.
-    lagsData0 = lagsData[:,:,:,0]
-    lagsData0 = sp.tile(lagsData0[:,:,:,sp.newaxis],(1,1,1,Nlags))
-    
-    lagsNoise0 = lagsNoise[:,:,:,0]
-    lagsNoise0 = sp.tile(lagsNoise0[:,:,:,sp.newaxis],(1,1,1,Nlags))
-    sigS = sp.sqrt(sp.absolute(lagsData)**2+sp.absolute(lagsData0)**2+sp.absolute(lagsNoise)**2 + sp.absolute(lagsNoise0)**2)/sp.sqrt(pulses)
+
     # multiply the data and the sigma by inverse of the scaling from the radar
     lagsData = lagsData*radar2acfmult
-    sigS = sigS*radar2acfmult
+    lagsNoise = lagsNoise*radar2acfmult
 
     # Apply summation rule
     # lags transposed from (time,beams,range,lag)to (range,lag,time,beams)
     lagsData = sp.transpose(lagsData,axes=(2,3,0,1))
-    sigS = sp.transpose(sigS,axes=(2,3,0,1))
+    lagsNoise = sp.transpose(lagsNoise,axes=(2,3,0,1))
     lagsDatasum = sp.zeros((Nrng2,Nlags,Nt,Nbeams),dtype=lagsData.dtype)
-    sigsSsum = sp.zeros((Nrng2,Nlags,Nt,Nbeams),dtype=sigS.dtype)
+    lagsNoisesum = sp.zeros((Nrng2,Nlags,Nt,Nbeams),dtype=lagsNoise.dtype)
     for irngnew,irng in enumerate(sp.arange(minrg,maxrg)):
         for ilag in range(Nlags):
             lagsDatasum[irngnew,ilag] = lagsData[irng+sumrule[0,ilag]:irng+sumrule[1,ilag]+1,ilag].sum(axis=0)
-            sigsSsum[irngnew,ilag] =sp.sqrt(sp.power(sigS[irng+sumrule[0,ilag]:irng+sumrule[1,ilag]+1,ilag],2).sum(axis=0))
+            lagsNoisesum[irngnew,ilag] = lagsNoise[irng+sumrule[0,ilag]:irng+sumrule[1,ilag]+1,ilag].sum(axis=0)
     # Put everything in a parameter list
     Paramdata = sp.zeros((Nbeams*Nrng2,Nt,Nlags),dtype=lagsData.dtype)
     # Put everything in a parameter list
-    Paramdatasig = sp.zeros((Nbeams*Nrng2,Nt,Nlags),dtype=sigsSsum.dtype)
     # transpose from (range,lag,time,beams) to (beams,range,time,lag)
     lagsDataSum = sp.transpose(lagsDatasum,axes=(3,0,2,1))
-    sigsSsum = sp.transpose(sigsSsum,axes=(3,0,2,1))
+    lagsNoiseSum = sp.transpose(lagsNoisesum,axes=(3,0,2,1))
+    # Get the covariance matrix
+    pulses_s=sp.transpose(pulses,axes=(1,2,0,3))[:,:Nrng2]
+    R=sp.transpose(lagsDataSum/sp.sqrt(2.*pulses_s),axes=(3,0,1,2))
+    Rw=sp.transpose(lagsNoiseSum/sp.sqrt(2.*pulses_s),axes=(3,0,1,2))
+    l=sp.arange(Nlags)
+    T1,T2=sp.meshgrid(l,l)
+    R0=R[sp.zeros_like(T1)]
+    Rw0=Rw[sp.zeros_like(T1)]
+    Td=sp.absolute(T1-T2)
+    Tl = T1>T2
+    R12 =R[Td]
+    R12[Tl]=sp.conjugate(R12[Tl])
+    Rw12 =Rw[Td]
+    Rw12[Tl]=sp.conjugate(Rw12[Tl])
+    Ctt=R0*R12+R[T1]*sp.conjugate(R[T2])+Rw0*Rw12+Rw[T1]*sp.conjugate(Rw[T2])
+    Cttout = sp.transpose(Ctt,(2,3,4,0,1))
+    Paramdatasig = sp.zeros((Nbeams*Nrng2,Nt,Nlags,Nlags),dtype=Cttout.dtype)
+
     curloc = 0
     for irng in range(Nrng2):
         for ibeam in range(Nbeams):
             Paramdata[curloc] = lagsDataSum[ibeam,irng].copy()
-            Paramdatasig[curloc] = sigsSsum[ibeam,irng].copy()
+            Paramdatasig[curloc] = Cttout[ibeam,irng].copy()
             curloc+=1
     ionodata = IonoContainer(coordlist,Paramdata,times = time_vec,ver =1, paramnames=sp.arange(Nlags)*sensdict['t_s'])
-    ionosigs = IonoContainer(coordlist,Paramdatasig,times = time_vec,ver =1, paramnames=sp.arange(Nlags)*sensdict['t_s'])
+    ionosigs = IonoContainer(coordlist,Paramdatasig,times = time_vec,ver =1, paramnames=sp.arange(Nlags*Nlags).reshape(Nlags,Nlags)*sensdict['t_s'])
     return (ionodata,ionosigs)
 
 