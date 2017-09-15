2c0e832ec14205f60e9120fb8fdc8d95b57bf3c9
John Swoboda <swoboj@bu.edu>
2017-09-15 15:53:44 -0400

    fixed problem with initialization of stats data

--- Test/statstest.py
+++ Test/statstest.py
@@ -230,7 +230,7 @@ def makehistdata(params,maindir):
     times = Ionofit.Time_Vector
 
     dataloc = Ionofit.Sphere_Coords
-    rng = dataloc[:,0]
+    rng = dataloc[:, 0]
     rng_log = sp.logical_and(rng > 200., rng < 400)
     dataloc_out = dataloc[rng_log]
     pnames = Ionofit.Param_Names
@@ -262,8 +262,8 @@ def makehistdata(params,maindir):
         if curparm == 'nepow':
             curparm = 'ne'
         datalist = []
-        for itn,itime in enumerate(times):
-            for iplot,filenum in enumerate(time2files[itn]):
+        for itn, itime in enumerate(times):
+            for  filenum in time2files[itn]:
                 filenum = int(filenum)
                 if curfilenum != filenum:
                     curfilenum = filenum
@@ -302,14 +302,14 @@ def configfilesetup(testpath,npulses):
     curloc = Path(__file__).resolve().parent
     defcon = curloc.joinpath('statsbase.ini')
 
-    (sensdict,simparams) = readconfigfile(defcon)
+    (sensdict, simparams) = readconfigfile(defcon)
     tint = simparams['IPP']*npulses
     ratio1 = tint/simparams['Tint']
-    simparams['Tint']=ratio1 * simparams['Tint']
+    simparams['Tint'] = ratio1 * simparams['Tint']
     simparams['Fitinter'] = ratio1 * simparams['Fitinter']
     simparams['TimeLim'] = ratio1 * simparams['TimeLim']
 
-    simparams['startfile']='startfile.h5'
+    simparams['startfile'] = 'startfile.h5'
     makeconfigfile(testpath.joinpath('stats.ini'),simparams['Beamlist'],sensdict['Name'],simparams)
 
 def makedata(testpath):
@@ -323,21 +323,22 @@ def makedata(testpath):
     finalpath = testpath.joinpath('Origparams')
     if not finalpath.exists():
         finalpath.mkdir()
-    data=SIMVALUES
-    z = sp.linspace(50.,1e3,50)
+    data = SIMVALUES
+    z = sp.linspace(50., 1e3, 50)
     nz = len(z)
-    params = sp.tile(data[sp.newaxis,sp.newaxis,:,:],(nz,1,1,1))
-    coords = sp.column_stack((sp.ones(nz),sp.ones(nz),z))
-    species=['O+','e-']
-    times = sp.array([[0,1e3]])
-    vel = sp.zeros((nz,1,3))
-    Icont1 = IonoContainer(coordlist=coords,paramlist=params,times = times,sensor_loc = sp.zeros(3),ver =0,coordvecs =
-        ['x','y','z'],paramnames=None,species=species,velocity=vel)
+    params = sp.tile(data[sp.newaxis, sp.newaxis, :, :], (nz, 1, 1, 1))
+    coords = sp.column_stack((sp.ones(nz), sp.ones(nz), z))
+    species = ['O+', 'e-']
+    times = sp.array([[0, 1e9]])
+    vel = sp.zeros((nz, 1, 3))
+    Icont1 = IonoContainer(coordlist=coords, paramlist=params, times=times,
+                           sensor_loc=sp.zeros(3), ver=0, coordvecs=['x', 'y', 'z'],
+                           paramnames=None, species=species, velocity=vel)
 
     finalfile = finalpath.joinpath('0 stats.h5')
     Icont1.saveh5(str(finalfile))
     # set start temp to 1000 K.
-    Icont1.Param_List[:,:,:,1]=1e3
+    Icont1.Param_List[:,:,:,1] = 1e3
     Icont1.saveh5(str(testpath.joinpath('startfile.h5')))
 
 
