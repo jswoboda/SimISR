6c69cf3b69732d5e11a7c04c15e20d19c02b9d07
John Swoboda <swoboj@bu.edu>
2018-04-03 13:54:14 -0400

    small fix

--- SimISR/utilFunctions.py
+++ SimISR/utilFunctions.py
@@ -683,7 +683,7 @@ def readconfigfile(fname):
         config.read(str(fname))
         beamlist = config.get('section 1','beamlist').split()
         beamlist = [float(i) for i in beamlist]
-        angles = angles = sensconst.getangles(dictlist[0]['beamlist'], dictlist[0]['radarname'])
+        angles = sensconst.getangles(dictlist[0]['beamlist'], dictlist[0]['radarname'])
         ang_data = sp.array([[iout[0],iout[1]] for iout in angles])
 
         sensdict = sensconst.getConst(config.get('section 1','radarname'),ang_data)
