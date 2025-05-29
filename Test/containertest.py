ea384213c84d266f251f20b24ae8be981bd8372a
John Swoboda <swoboj@bu.edu>
2025-05-29 13:58:44 -0400

    update the containertest.py

--- Test/containertest.py
+++ Test/containertest.py
@@ -5,41 +5,8 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 from SimISR.testfunctions import chapman_func, temp_profile
-from SimISR import IonoContainer,make_iline_specds
+from SimISR import make_iline_specds
 
-
-def example_iono():
-    # make a cube
-
-    xy = np.linspace(-1000,1000,200)
-    z = np.linspace (50,1000,100)
-
-    xm,ym,zm = np.meshgrid(xy,xy,z)
-    coords = np.zeros((xm.size, 3))
-    coords[:, 0] = xm.flatten()
-    coords[:, 1] = ym.flatten()
-    coords[:, 2] = zm.flatten()
-    N_0=1e11
-    z_0=250.0
-    H_0=50.0
-
-    Ne_profile = chapman_func(zm.flatten(), H_0, z_0, N_0)
-    (Te, Ti) = temp_profile(zm.flatten())
-    times=np.array([[0, 1e6]])
-
-    (Nlocs, ndims) = coords.shape
-    Ntime = len(times)
-    vel = np.zeros((Nlocs, Ntime, ndims))
-    species = ["O+", "e-"]
-    # put the parameters in order
-    params = np.zeros((Ne_profile.size, len(times), 2, 2))
-    params[:, :, 0, 1] = np.repeat(Ti[:, np.newaxis], Ntime, axis=1)
-    params[:, :, 1, 1] = np.repeat(Te[:, np.newaxis], Ntime, axis=1)
-    params[:, :, 0, 0] = np.repeat(Ne_profile[:, np.newaxis], Ntime, axis=1)
-    params[:, :, 1, 0] = np.repeat(Ne_profile[:, np.newaxis], Ntime, axis=1)
-
-    iono = IonoContainer(coords,params,times,species = species,velocity=vel)
-    return iono
 def example_xr():
 
     xy = np.linspace(-500,500,50)
@@ -143,7 +110,7 @@ def create_spectrum(i_ds):
     sf = 50e3
     nfft = int(2**10)
     f_vec = np.fft.fftshift(np.fft.fftfreq(nfft,d=1/sf))
-    spec_args = dict(centerFrequency=440.2e6,sampfreq=sf,f=f_vec)
+    spec_args = dict(centerFrequency=cf,sampfreq=sf,f=f_vec)
     ilineds = make_iline_specds(i_ds,spec_args)
     return ilineds
 
